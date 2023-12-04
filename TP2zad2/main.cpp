c5d58f5f7924e8897673e20c0c289f29b80dc97c
Viktoria Aisuvakova <semenova.vika19@yandex.ru>
2023-12-04 13:05:11 +0300

    main2

--- TP2zad2/main.cpp
+++ TP2zad2/main.cpp
@@ -1,6 +1,6 @@
 
 #include <iostream>
-#include <fstream>
+#include <fstream> 
 #include <sstream>
 
 using namespace std;
@@ -8,15 +8,20 @@ using namespace std;
 int main() {
     ifstream file("test.txt");
 
-    string line;
-    while (getline(file, line)) {
-        istringstream iss(line);
+    string line; //ќбъ€вл€ет строку дл€ хранени€ текущей строки из файла
+    if (!file.is_open())
+        cout << "error" << endl;
+    else
+    {
+        while (getline(file, line)) { //Ёто считывает строки из файла,пока не будет достигнут конец файла.
+            istringstream iss(line); //создает строковый поток из текущей строки
 
-        string word;
-        while (iss >> word) {
-            if (word.size() == 2 && isdigit(word[0]) && isdigit(word[1])) {
-                cout << line << endl;
-                break;
+            string word; 
+            while (iss >> word) { //считывает слова из этого строкового потока до конца строки
+                if (word.size() == 2 && isdigit(word[0]) && isdigit(word[1])) {
+                    cout << line << endl; //он печатает текущую строку и выходит из цикла
+                    break;
+                }
             }
         }
     }
@@ -25,7 +30,7 @@ int main() {
 }
 
 //#include <iostream>
-//#include <fstream> 
+//#include <fstream>
 //#include <sstream>
 //
 //using namespace std;
@@ -35,42 +40,48 @@ int main() {
 //    int flag = 0;
 //    int flag1 = 0;
 //    string line;
-//    while (getline(file, line))
+//    if (!file.is_open())
+//        cout << "error" << endl;
+//    else
 //    {
-//        flag = 0;
-//        istringstream iss(line);
-//
-//        string word;
-//        while (iss >> word)
+//        while (getline(file, line))
 //        {
-//            for (int i = 0; i < word.size(); i++)
+//            flag = 0;
+//            istringstream iss(line);
+//
+//            string word;
+//
+//            while (iss >> word)
 //            {
-//                if (!(isdigit(word[i])))
-//                    flag1 = 0;
-//                if ((isdigit(word[i])) && flag1 == 0)
+//                for (int i = 0; i < word.size(); i++)
 //                {
-//                    i++;
-//                    if (isdigit(word[i]))
+//                    if (!(isdigit(word[i])))
+//                        flag1 = 0;
+//                    if ((isdigit(word[i])) && flag1 == 0)
 //                    {
 //                        i++;
-//                        if (!(isdigit(word[i])))
+//                        if (isdigit(word[i]))
 //                        {
-//                            cout << line << endl;
-//                            flag = 1;
+//                            i++;
+//                            if (!(isdigit(word[i]))) 
+//                            {
+//                                cout << line << endl;
+//                                flag = 1;
 //
+//                            }
+//                            else
+//                                flag1 = 1;
 //                        }
-//                        else
-//                            flag1 = 1;
 //                    }
+//                    if (flag == 1)
+//                        break;
 //                }
 //                if (flag == 1)
 //                    break;
 //            }
-//            if (flag == 1)
-//                break;
+//
+//
 //        }
-//
-//
 //    }
 //    return 0;
 //}
